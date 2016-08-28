f73d134c590bfc51e895ce17829846acc0c9226c
Podkopaev Anton <podkoav239@gmail.com>
2016-08-28 15:59:49 +0300

    Small refactoring.

--- regression/testFormatList.ml
+++ regression/testFormatList.ml
@@ -1,71 +1,8 @@
-open FormatList
 open Printf
-
-module Expr =
-  struct
-    type t = Var   of string
-           | Cons  of int
-           | Binop of string * t * t
-
-    let rec to_format_list = function
-      | Var  s -> !s
-      | Cons n -> !(string_of_int n)
-      | Binop (c, t1, t2) ->
-         let f1  = (to_format_list t1) in
-         let f2  = (to_format_list t2) in
-         (f1 >||< !c >||< f2) >?<
-           (f1 >-< ((2 >> !c) >||< f2))
-  end
-
-module Stmt =
-  struct
-    type t = Read   of string
-           | Write  of Expr.t
-           | Assign of string * Expr.t
-           | Seq    of t list 
-           | If     of Expr.t * t * t
-           | While  of Expr.t * t
-
-    let rec to_format_list = function
-      | Read    s    -> !"read("  >|< !s >|< !")" 
-      | Write   e    -> !"write(" >|< (Expr.to_format_list e) >|< !")" 
-      | Assign (s,e) -> !s >||< !":=" >||< (Expr.to_format_list e) 
-      | Seq     []   -> initial
-      | Seq (hd::tl) ->
-         List.fold_left (fun fs s ->
-             let f = to_format_list s in
-             (fs >|< !";" >||< f) >?<
-               (fs >|< !";" >-< f)
-           ) (to_format_list hd) tl
-      | If (e,s1,s2) ->
-         let e  = Expr.to_format_list e in
-         let f1 = to_format_list s1 in
-         let f2 = to_format_list s2 in
-         (!"if" >||< (e ^ 1) >||< !"then" >||< (f1 ^ 1) >||< !"else" >||< (f2 ^ 1)) >?<
-         (!"if" >||< e >-<
-            !"then" >||< f1 >-<
-            !"else" >||< f2)
-      | While (e,body) ->
-         let e     = Expr.to_format_list e in
-         let bodyf = to_format_list body in
-         (!"while" >||< (e ^ 1) >||< !"do" >||< (bodyf ^ 1) >||< !"od") >?<
-         (!"while" >||< e     >-<
-            !"do"  >||< bodyf >-<
-            !"od")
-  end
-                             
-exception Not_Operation 
-
-let op = function
-  | '+' -> (+)
-  | '-' -> (-)
-  | '*' -> ( * )
-  | '/' -> (/)
-  | '%' -> (mod)
-  | _   -> raise Not_Operation
-
-open Stmt
-open Expr
+open FormatList
+open While
+open While.Stmt
+open While.Expr
 
 let expr00 = Var  "n"
 let expr03 = Cons 1 
@@ -82,12 +19,12 @@ let term01 =
        Write (Var "res")]
 
 let testStmt n term =
-  default_width := n;
+  FormatList.default_width := n;
   Printf.printf "%s\n\n" @@
     to_string (Stmt.to_format_list term)
 
 let testExpr n term =
-  default_width := n;
+  FormatList.default_width := n;
   Printf.printf "%s\n\n" @@
     to_string (Expr.to_format_list term)
 
@@ -99,4 +36,3 @@ let _ =
     testStmt 15 term01
   with Failure s ->
     Printf.printf "Ok\n\n"
-
---
+++ src/while.ml
@@ -0,0 +1,65 @@
+open FormatList
+open Printf
+
+module Expr =
+  struct
+    type t = Var   of string
+           | Cons  of int
+           | Binop of string * t * t
+
+    let rec to_format_list = function
+      | Var  s -> !s
+      | Cons n -> !(string_of_int n)
+      | Binop (c, t1, t2) ->
+         let f1  = (to_format_list t1) in
+         let f2  = (to_format_list t2) in
+         (f1 >||< !c >||< f2) >?<
+           (f1 >-< ((2 >> !c) >||< f2))
+  end
+
+module Stmt =
+  struct
+    type t = Read   of string
+           | Write  of Expr.t
+           | Assign of string * Expr.t
+           | Seq    of t list 
+           | If     of Expr.t * t * t
+           | While  of Expr.t * t
+
+    let rec to_format_list = function
+      | Read    s    -> !"read("  >|< !s >|< !")" 
+      | Write   e    -> !"write(" >|< (Expr.to_format_list e) >|< !")" 
+      | Assign (s,e) -> !s >||< !":=" >||< (Expr.to_format_list e) 
+      | Seq     []   -> initial
+      | Seq (hd::tl) ->
+         List.fold_left (fun fs s ->
+             let f = to_format_list s in
+             (fs >|< !";" >||< f) >?<
+               (fs >|< !";" >-< f)
+           ) (to_format_list hd) tl
+      | If (e,s1,s2) ->
+         let e  = Expr.to_format_list e in
+         let f1 = to_format_list s1 in
+         let f2 = to_format_list s2 in
+         (!"if" >||< (e ^ 1) >||< !"then" >||< (f1 ^ 1) >||< !"else" >||< (f2 ^ 1)) >?<
+         (!"if" >||< e >-<
+            !"then" >||< f1 >-<
+            !"else" >||< f2)
+      | While (e,body) ->
+         let e     = Expr.to_format_list e in
+         let bodyf = to_format_list body in
+         (!"while" >||< (e ^ 1) >||< !"do" >||< (bodyf ^ 1) >||< !"od") >?<
+         (!"while" >||< e     >-<
+            !"do"  >||< bodyf >-<
+            !"od")
+  end
+                             
+exception Not_Operation 
+
+let op = function
+  | '+' -> (+)
+  | '-' -> (-)
+  | '*' -> ( * )
+  | '/' -> (/)
+  | '%' -> (mod)
+  | _   -> raise Not_Operation
